c7f8390dbe1ff3d8e6204a9e325b1899f6e13b37
vovatran1993 <vovatran1993@gmail.com>
2019-08-03 12:44:21 -0500

    gw

--- igw.tf
+++ igw.tf
@@ -1,8 +1,8 @@
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.dev.id}"
 
   tags = {
-    Name = "${var.Name}"
+      Name = "${var.Name}"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"
