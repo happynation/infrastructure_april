455528c3ede057771c4328c73ba0418a86134290
vovatran1993 <vovatran1993@gmail.com>
2019-08-03 11:35:11 -0500

    dfdf

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "VPC" {
     value = "${aws_vpc.dev.id}"
+
+    output "public_subnet" {
+  value = "${aws_subnet.public.id}"
 }
--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,25 @@
 resource "aws_subnet" "public" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name    =        "Wordpress"
-      Env    =         "Dev"
-      Created_by    =  "Vova"
-      Dept    =        "IT"
+      Name = "${var.Name}.public"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
+
+
+
+resource "aws_subnet" "private" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+      Name = "${var.Name}.private"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
   }
 }
