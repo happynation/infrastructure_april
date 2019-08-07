6c887b89718fdec4e6f8599a63c61d9dc613bc37
vovatran1993 <vovatran1993@gmail.com>
2019-08-07 13:31:29 -0500

    removed bucket

--- bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "rootvova-april1" {
-    bucket = "infra-april-vova-2019a"
-
-  
-}
--- dont_create.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "rootvova-april2" {
-    bucket = "infra-april-vova-2019b"
-    
-  
-}
